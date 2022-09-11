f500267a24239a10e60c77a987fbf264b36a0ae1
Huw Lewis <hlewis@qmetric.co.uk>
2022-09-11 11:20:48 +0100

    Add test infrastructure for an in-memory db

--- src/test/groovy/org/huwtl/pgrepl/consumer/PostgresDataChangeConsumerTest.groovy
+++ src/test/groovy/org/huwtl/pgrepl/consumer/PostgresDataChangeConsumerTest.groovy
@@ -1,11 +1,17 @@
 package org.huwtl.pgrepl.consumer
 
+import org.huwtl.pgrepl.db.EmbeddedDatabase
 import org.huwtl.pgrepl.publisher.InMemoryPublishedDataStore
 import spock.lang.AutoCleanup
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
 
 class PostgresDataChangeConsumerTest extends Specification {
+    @Shared
+    @AutoCleanup
+    private final database = new EmbeddedDatabase()
+
     @AutoCleanup
     private final dataStore = new InMemoryPublishedDataStore<String>()
     private final consumer = new PostgresDataChangeConsumer(dataStore)
